327072ba42cbb498444a0366ebd3edaab60bebda
Mohammad Sadeghi <moh02sadeghi@yahoo.com>
2023-05-27 16:49:54 +0330

    send user and groups to dashboard.html

--- website/views/dashboard_view.py
+++ website/views/dashboard_view.py
@@ -6,3 +6,10 @@ from django.views.generic import TemplateView
 @method_decorator(login_required, name="dispatch")
 class DashboardView(TemplateView):
     template_name = "dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['groups'] = self.request.user.group_set.all()
+
+        return context
