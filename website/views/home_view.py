53c18ad655c2666ace2297fccdb517e602bc93b3
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2023-06-08 18:18:25 +0330

    feat(home): add better html and css

--- website/views/home_view.py
+++ website/views/home_view.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
+from website.models import User, Group, Transaction, Cost
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            "users_count": User.objects.all().count(),
+            "groups_count": Group.objects.all().count(),
+            "transactions": Transaction.objects.all().count(),
+            "costs": Cost.objects.all().count()
+        }
