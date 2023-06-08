00ba71141163d1c8e70667f4d01ada724e7568c4
Mohammad Sadeghi <moh02sadeghi@yahoo.com>
2023-06-08 21:38:03 +0330

    add additional data to dashboard

--- website/views/dashboard_view.py
+++ website/views/dashboard_view.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
-from website.models import User
+from website.models import User, Cost
 
 
 @method_decorator(login_required, name="dispatch")
@@ -11,8 +11,8 @@ class DashboardView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         user = User.objects.get(id=self.request.user.id)
+
         context['username'] = user.username
         context['groups'] = user.group_set.all().prefetch_related('costs_set')
         if user.debt and user.credit:
@@ -23,4 +23,7 @@ class DashboardView(TemplateView):
             context['owe'] = 0.0
             context['owed'] = 0.0
             context['total_balance'] = 0.0
+        context['owe_costs'] = Cost.objects.filter(members_set__user=user, members_set__paid=False)
+        context['owed_costs'] = user.paid_costs.filter(members_set__paid=False)
+
         return context
