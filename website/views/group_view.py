2ba015b6888fecfe2c0b6fc5fe47b2f789e66b1f
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2023-06-13 01:41:55 +0330

    shit shit code

--- website/views/group_view.py
+++ website/views/group_view.py
@@ -78,10 +78,11 @@ class GroupDetailView(DetailView):
         only = self.request.GET.get('only', 'off') == 'on'
         context['only'] = only
 
+        user = User.objects.get(id=user_id)
+        context['username'] = user.username
         if only:
             context['costs'] = group.costs_set.filter(archived=archived, members_set__user__id=user_id,
                                                       members_set__paid=paid).order_by(sort_by)
         else:
             context['costs'] = group.costs_set.filter(archived=archived).order_by(sort_by)
-
         return context
